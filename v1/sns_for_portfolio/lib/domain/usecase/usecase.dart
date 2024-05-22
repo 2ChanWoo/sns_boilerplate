3d258d1877db3d912e4fa126f10bb78a33f00fd5
LCW <2chanwoo@groundk.com>
2024-05-22 10:50:17 +0900

    Domain(UseCase): Changed UseCase structure

--- v1/sns_for_portfolio/lib/domain/usecase/usecase.dart
+++ v1/sns_for_portfolio/lib/domain/usecase/usecase.dart
@@ -1,9 +1,23 @@
 const Map<String, dynamic> emptyMap = {};
 
-abstract class UseCase<T> {
-  Future<T> execute({Map<String, dynamic> body = emptyMap, Map<String, dynamic> path = emptyMap});
+// class Params<T> {
+//   T? body;
+//   // Map<String, String>? param;
+//   Map<String, String>? query;
+//   Map<String, String>? path;
+//
+//   Params({this.body, this.query, this.path});
+// }
+
+/// [T] Unboxing 'Response' of [execute]
+/// [P] 'Request' parameters of [execute]
+abstract class UseCase<T, P> {
+  Future<T> execute(P params);
 }
 
-abstract class StreamUseCase<T> {
-  Stream<T> stream({Map<String, dynamic> param = emptyMap});
+/// (Part of override) if not required params~
+// Future<QuoteListPage> execute([void _]) async {} //TODO: => is it Non-abstract?
+
+abstract class StreamUseCase<T, P> {
+  Stream<T> stream(P params);
 }
